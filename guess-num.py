09f7bb6154c164fce7eff3ed29ffde60509558b6
Allen Teng <allen70213@gmail.com>
2022-12-28 16:26:42 +0800

    count

--- guess-num.py
+++ guess-num.py
@@ -1,14 +1,18 @@
 import random
 
 num = random.randint(1, 100)
+count = 0
 while True:
     ans = input('請輸入數字:')
     ans = int(ans)
+    count = count + 1
     if ans == num:
         print('你猜對了')
+        print('總共花了', count, '次')
         break
     else:
         if ans < num:
          print('比答案小')
         else:
          print('比答案大')
+    print('這是你猜的第', count, '次')
